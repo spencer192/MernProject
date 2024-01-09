60a16ee7a45e91c94cf36f97682567aee60610bc
spencer192 <spencerdww11@gmail.com>
2024-01-09 20:56:12 +0500

    Project push on git hub

---
+++ server/data/apiData.js
@@ -0,0 +1,44 @@
+const notes =[
+    {
+        id: "1",
+        name: "Shahbaz",
+        age: "23",
+        married: "None",
+        email: "shahbaz@gmail.com",
+        designation: "Software Enginear",
+    },
+    {
+        id: "2",
+        name: "Hassan",
+        age: "23",
+        married: "None",
+        email: "hasan@gmail.com",
+        designation: "Software Enginear",
+    },
+    {
+        id: "3",
+        name: "dawood",
+        age: "23",
+        married: "None",
+        email: "dawood@gmail.com",
+        designation: "Software Enginear",
+    },
+    {
+        id: "4",
+        name: "yousaf",
+        age: "23",
+        married: "None",
+        email: "yousaf@gmail.com",
+        designation: "Software Enginear",
+    },
+    {
+        id: "5",
+        name: "Ali",
+        age: "23",
+        married: "None",
+        email: "Ali@gmail.com",
+        designation: "Software Enginear",
+    }
+];
+
+module.exports = notes;
---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const notes = require("./data/apiData");
+const dotenv= require("dotenv")
+
+const app = express();
+dotenv.config();
+
+app.get('/', (req, res) => {
+    res.send("Api Is Runing....")
+})
+
+app.get("/api/notes", (req, res) =>{
+    res.json(notes);
+})
+
+app.get("/api/notes/:id", (req, res) =>{
+    const note = notes.find((n) => n.id === req.params.id);
+    res.send(note);
+})
+
+const PORT= process.env.PORT || 5001;
+
+app.listen(PORT, console.log(`Server Start with port ${PORT}`))
